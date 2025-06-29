7a5398a3394b3b6a4b1cb77d229e3a06779cde12
Uche Onyejide <ucheonyejide@Uches-MacBook-Air.local>
2025-06-29 09:44:46 -0400

    fixed port

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors({
 
 app.use(cookieParser()); // Middleware to parse cookies
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 dotenv.config();
 app.use(express.json());
