5d2e995d473ed5c71cf38a91bbbc3caf1b454cef
Uche Onyejide <ucheonyejide@Mac.ht.home>
2025-06-30 21:24:23 -0400

    removed baseurl

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 
 // Configure Axios to include cookies in all requests
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:4000'; // Update with your backend URL
+// axios.defaults.baseURL = 'http://localhost:4000'; // Update with your backend URL
